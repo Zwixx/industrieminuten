95bfdce62371fbcaeee4389e5a03d2e77f3092c9
Zwixx <zwiixx@googlemail.com>
2023-11-26 20:20:46 +0100

    Anpassungen Design

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <main>
-    <h3>Industriestundenrechner</h3>
+    
     <table>
+      <caption>Industriestundenrechner</caption>
+      <thead>
         <tr>
-          <td>Tag</td>
-          <td>Beginn</td>
-          <td>Ende</td>
-          <td>Pause</td>
-          <td>Industriestunden / Zeit</td>
+          <th>Tag</th>
+          <th>Beginn</th>
+          <th>Ende</th>
+          <th>Pause</th>
+          <th>Industriestunden / Zeit</th>
         </tr>
-      <Zeitzeile day="Mo" @updateSumme="updateStunden1"/>
-      <Zeitzeile day="Di" @updateSumme="updateStunden2"/>
-      <Zeitzeile day="Mi" @updateSumme="updateStunden3"/>
-      <Zeitzeile day="Do" @updateSumme="updateStunden4"/>
-      <Zeitzeile day="Fr" @updateSumme="updateStunden5"/>
-      <Zeitzeile day="Sa" @updateSumme="updateStunden6"/>
-      <Zeitzeile day="So" @updateSumme="updateStunden7"/>
-      <tr><td colspan="5">Summe: {{ total }}</td></tr>
+      </thead>
+      <tbody>
+        <Zeitzeile day="Mo" @updateSumme="updateStunden1"/>
+        <Zeitzeile day="Di" @updateSumme="updateStunden2"/>
+        <Zeitzeile day="Mi" @updateSumme="updateStunden3"/>
+        <Zeitzeile day="Do" @updateSumme="updateStunden4"/>
+        <Zeitzeile day="Fr" @updateSumme="updateStunden5"/>
+        <Zeitzeile day="Sa" @updateSumme="updateStunden6"/>
+        <Zeitzeile day="So" @updateSumme="updateStunden7"/>
+        <tr><td colspan="4" align="right">Summe:</td><td>{{ total }}</td></tr>
+      </tbody>
     </table>
   </main>
 </template>
@@ -37,38 +42,38 @@ export default {
       total5: 0,
       total6: 0,
       total7: 0,
-      total: 0,
+      total: "",
     };
   },
   methods: {
     updateStunden1(value : number) {
       this.total1 = value;
-      this.total = this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7;
+      this.total = (this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7).toFixed(2);
       console.log(this.total)
     },
     updateStunden2(value : number) {
       this.total2 = value;
-      this.total = this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7;
+      this.total = (this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7).toFixed(2);
     },
     updateStunden3(value : number) {
       this.total3 = value;
-      this.total = this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7;
+      this.total = (this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7).toFixed(2);
     },
     updateStunden4(value : number) {
       this.total4 = value;
-      this.total = this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7;
+      this.total = (this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7).toFixed(2);
     },
     updateStunden5(value : number) {
       this.total5 = value;
-      this.total = this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7;
+      this.total = (this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7).toFixed(2);
     },
     updateStunden6(value : number) {
       this.total6 = value;
-      this.total = this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7;
+      this.total = (this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7).toFixed(2);
     },
     updateStunden7(value : number) {
       this.total7 = value;
-      this.total = this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7;
+      this.total = (this.total1 + this.total2 + this.total3 + this.total4 + this.total5 + this.total6 + this.total7).toFixed(2);
     },
   }
 };
