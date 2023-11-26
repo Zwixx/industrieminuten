0ce02a8ad7e64e80dff51704609bc1c220e8bea4
Zwixx <zwiixx@googlemail.com>
2023-11-26 20:37:47 +0100

    Design auf scss umgestelt.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 import App from './App.vue';
 
-import './assets/main.css'
+import './assets/main.scss'
 
 createApp(App).use(PrimeVue).mount('#app')
 
